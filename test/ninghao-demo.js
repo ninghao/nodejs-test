0b9231f705054db0c5536b07c9c900d5b7eb13e7
wanghao <117663444@qq.com>
2016-09-01 09:43:40 +0800

    mocha：测试框架

---
+++ test/ninghao-demo.js
@@ -0,0 +1,12 @@
+describe('NinghaoDemo', function () {
+  describe('方法 1', function () {
+    context('情境 1', function () {
+      it('测试 1', function () {
+
+      })
+      it('测试 2', function () {
+
+      })
+    })
+  })
+})
