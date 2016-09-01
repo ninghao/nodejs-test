73d30e1111f5c469773ebc2de6d693158fa6289b
wanghao <117663444@qq.com>
2016-09-01 10:39:40 +0800

    让测试通过

--- test/ninghao-demo.js
+++ test/ninghao-demo.js
@@ -1,5 +1,8 @@
 const chai = require('chai')
 const expect = chai.expect
+const NinghaoDemo = require('../lib/ninghao-demo')
+
+var demo = new NinghaoDemo()
 
 describe('NinghaoDemo', function () {
   it('单价是 10 块钱的 3 件商品小计金额应该是 30 块', function () {
