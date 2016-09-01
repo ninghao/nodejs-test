441cf566986d043e7bab41190bab4f92cb35d993
wanghao <117663444@qq.com>
2016-09-01 10:51:58 +0800

    测试异步的方法：setTimeout

--- test/ninghao-demo.js
+++ test/ninghao-demo.js
@@ -5,8 +5,17 @@ const NinghaoDemo = require('../lib/ninghao-demo')
 var demo = new NinghaoDemo()
 
 describe('NinghaoDemo', function () {
-  it('单价是 10 块钱的 3 件商品小计金额应该是 30 块', function () {
-    var subtotal = demo.subtotal(10, 3)
-    expect(subtotal).to.equal(30)
+  this.timeout(5000)
+
+  it('一段时间以后返回数据', function (done) {
+    demo.watiTwoSecond('hello', function (data) {
+      expect(data).to.equal('hello')
+      done()
+    })
   })
+
+  // it('单价是 10 块钱的 3 件商品小计金额应该是 30 块', function () {
+  //   var subtotal = demo.subtotal(10, 3)
+  //   expect(subtotal).to.equal(30)
+  // })
 })
