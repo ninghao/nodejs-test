a252d37e4a21424241ac65a6fa58c39fdce644fb
wanghao <117663444@qq.com>
2016-09-01 09:48:34 +0800

    mocha：安排测试之前与之后要做的事

--- test/ninghao-demo.js
+++ test/ninghao-demo.js
@@ -1,5 +1,21 @@
 describe('NinghaoDemo', function () {
   describe('方法 1', function () {
+    before(function () {
+      console.log('--- 测试之前')
+    })
+
+    after(function () {
+      console.log('--- 测试之后')
+    })
+
+    beforeEach(function () {
+      console.log('------- 每条测试之前')
+    })
+
+    afterEach(function () {
+      console.log('------- 每条测试之后')
+    })
+
     context('情境 1', function () {
       it('测试 1', function () {
 
